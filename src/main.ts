880d758543690ed13b28497e046f7e73d53dae38
Wendy Cruz <wc1769338@gmail.com>
2026-06-04 22:32:38 -0400

    configurar cors para produccion

--- src/main.ts
+++ src/main.ts
@@ -9,16 +9,17 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());//habilitar el parser de cookies
-  app.enableCors({
-    origin: ['http://localhost:3000','http://localhost:3001'], 
-    credentials: true,
-    methods: 'GET,HEAD,POST,PUT,PATCH,DELETE, OPTIONS',
-    allowedHeaders: [
+app.enableCors({
+  origin: true,
+  credentials: true,
+  methods: 'GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS',
+  allowedHeaders: [
     'Content-Type',
     'Authorization',
     'X-CSRF-Token',
-  'x-reset-token',],
-  });
+    'x-reset-token',
+  ],
+});
 
   app.use('/uploads', express.static(join(process.cwd(), 'uploads')));
   
